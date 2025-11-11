f17613abd9af13dd1e2dadbe74df3fdf16bf94f0
stephane <stephanemaire71@gmail.com>
2025-11-11 16:08:31 +0100

    Exercice 6: Render list + conditional rendering

--- app/components/CityOverviewCard/index.tsx
+++ app/components/CityOverviewCard/index.tsx
@@ -7,6 +7,10 @@ import { Card } from "~/components/ui/Card";
 export type CityOverviewCardProps = CityOverviewProps;
 
 export const CityOverviewCard = (props: CityOverviewCardProps) => {
+  if (props.population < 100000) {
+    return null;
+  }
+
   return (
     <Card>
       <CityOverview {...props} />
