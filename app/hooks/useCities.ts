4410e02fbe019fcf7e417ee348820eea411b1190
Stephane Maire <stephanemaire71@gmail.com>
2025-11-16 20:58:46 +0100

    Exercice 11 bis: Add population filter on cities

--- app/hooks/useCities.ts
+++ app/hooks/useCities.ts
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { City, CityFiltersType } from "~/types";
+import { citiesFilter } from "~/utils/citiesFilter";
+
+const CITY_FILTER_DEFAULT = {
+  department: "",
+  populationMin: undefined,
+  populationMax: undefined,
+};
 
 export const useCities = () => {
   const [cities, setCities] = useState<City[]>([]);
-  const [filters, setFilters] = useState<CityFiltersType>({
-    department: "",
-  });
-  const [citiesFiltered, setCitiesFiltered] = useState<City[]>([]);
+  const [filters, setFilters] = useState<CityFiltersType>(CITY_FILTER_DEFAULT);
 
   useEffect(() => {
     const trigger = async () => {
@@ -18,16 +22,9 @@ export const useCities = () => {
     void trigger();
   }, []);
 
-  useEffect(() => {
-    if (!filters.department) {
-      return setCitiesFiltered(cities);
-    }
-
-    const filteredCities = cities.filter(
-      (city) => city.codeDepartement === filters.department,
-    );
-    setCitiesFiltered(filteredCities);
-  }, [cities, filters.department]);
+  const citiesFiltered = useMemo(() => {
+    return citiesFilter({ cities, filters });
+  }, [cities, filters]);
 
   const handleChangeFilters = (newFilters: CityFiltersType) => {
     setFilters(newFilters);
