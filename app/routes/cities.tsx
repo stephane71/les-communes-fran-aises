e85e6073464badac042ed3a393b4cb8e99862eb0
Stephane Maire <stephanemaire71@gmail.com>
2025-11-17 13:32:04 +0100

    Exercice 12: Add loading state when fetching data

--- app/routes/cities.tsx
+++ app/routes/cities.tsx
@@ -4,9 +4,10 @@ import type { City } from "~/types";
 import { NavLink, useNavigate } from "react-router";
 import { CityFilters } from "~/components/CityFilters";
 import { useCities } from "~/hooks/useCities";
+import { CityOverviewCardSkeleton } from "~/components/CityOverviewCard/CityOverviewCard.skeleton";
 
 const Cities = () => {
-  const { cities, filters, handleChangeFilters } = useCities();
+  const { cities, filters, handleChangeFilters, isLoading } = useCities();
 
   const navigate = useNavigate();
 
@@ -31,17 +32,25 @@ const Cities = () => {
           <CityFilters value={filters} onChange={handleChangeFilters} />
           <div>{`Nombre de communes: ${cities.length}`}</div>
         </div>
-        <div className="flex flex-col gap-2">
-          {(cities as City[]).map(({ nom, code, population }) => (
-            <CityOverviewCard
-              key={code}
-              name={nom}
-              code={code}
-              population={population}
-              onClick={handleClick}
-            />
-          ))}
-        </div>
+        {isLoading ? (
+          <div className="flex flex-col gap-2" aria-busy>
+            {Array.from({ length: 8 }).map((_, i) => (
+              <CityOverviewCardSkeleton key={i} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {(cities as City[]).map(({ nom, code, population }) => (
+              <CityOverviewCard
+                key={code}
+                name={nom}
+                code={code}
+                population={population}
+                onClick={handleClick}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
